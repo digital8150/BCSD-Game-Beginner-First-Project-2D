a6c8f8674dbcdefcbde409fb88dad82df262726a
Digital <73296205+digital8150@users.noreply.github.com>
2025-05-14 20:46:02 +0900

    feautre: 카메라 PostFX 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de3f132873b225611c2ffae90f129ebd45cfa6d86fe2b5fcebac95c341e6789e
-size 20217
+oid sha256:1fb5fc2736413290f28d67b18d609887568489bf07e886da387209a4d6754bea
+size 20315
