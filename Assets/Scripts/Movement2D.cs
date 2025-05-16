6fe28e1c3c961f7360a9d61a061a6163353fe692
Digital <73296205+digital8150@users.noreply.github.com>
2025-05-17 02:29:33 +0900

    chore: 빌드 설정

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2546886d23ae091fa30bb78b72303cab69915149848a3b843a641f9c526ad10d
-size 607
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 0.0f;
+    [SerializeField]
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float MoveSpeed => moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Update()
+    {
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    public void MoveTo(Vector3  direction)
+    {
+        moveDirection = direction;
+    }    
+}
